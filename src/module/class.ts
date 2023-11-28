fe5f8b992845a403f87572ec80b3c76f9cf7a41f
Sebastian Agudelo <sebasagudelo22@hotmail.com>
2023-11-28 14:34:41 +0100

    skapat en class

---
+++ src/module/class.ts
@@ -0,0 +1,9 @@
+class Products {
+    constructor (public id: number,
+        public productName: string,
+        public productSize: string,
+        public productPrice: number,
+        public productDescription: string,
+        public productimg: string){};
+}
+
